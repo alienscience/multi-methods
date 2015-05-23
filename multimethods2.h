c0df95da3bb0dcb24db9ac5e53e28d1f68f09536
Alienscience <saul@alienscience.org.uk>
2015-05-23 14:10:37 +0200

    Added 3 argument case.

--- multimethods2.h
+++ multimethods2.h
@@ -17,7 +17,8 @@ struct Builder
         virtual ArgList<TArgs...>* addArg(T& v) = 0;
     };
 
-    // An empty ArgList
+    //-------------------------------------------------------------------------
+
     template <typename... Ts> struct ArgStart : public ArgList<TArgs...> {};
 
     template <typename T, typename... Ts>
@@ -26,7 +27,8 @@ struct Builder
         virtual ArgList<TArgs...>* addArg(T& v);
     };
 
-    // A one argument ArgList
+    //-------------------------------------------------------------------------
+
     template <typename T0, typename... Ts>
     struct Arg0 : public ArgList<TArgs...>
     {
@@ -38,8 +40,44 @@ struct Builder
     struct Arg0<T0, T, Ts...> : public Arg0<T0,Ts...>
     {
         Arg0<T0,T,Ts...>(T0& v) : Arg0<T0,Ts...>(v) {}
+        virtual ArgList<TArgs...>* addArg(T& v);
+    };
+
+    //-------------------------------------------------------------------------
+
+    template <typename T0, typename T1, typename... Ts>
+    struct Arg1 : public Arg0<T0,Ts...>
+    {
+        T1& arg1_;
+        Arg1(T0& arg0, T1& v) : Arg0<T0,Ts...>(arg0), arg1_(v) {}
+    };
+
+    template <typename T0, typename T1, typename T, typename... Ts>
+    struct Arg1<T0, T1, T, Ts...> : public Arg1<T0,T1,Ts...>
+    {
+        Arg1<T0,T1,T,Ts...>(T0& arg0, T1& v) : Arg1<T0,T1,Ts...>(arg0,v) {}
+        virtual ArgList<TArgs...>* addArg(T& v);
+    };
+
+    //-------------------------------------------------------------------------
+
+    template <typename T0, typename T1, typename T2, typename... Ts>
+    struct Arg2 : public Arg1<T0,T1,Ts...>
+    {
+        T2& arg2_;
+        Arg2(T0& arg0, T1& arg1, T2& v) :
+            Arg1<T0,T1,Ts...>(arg0, arg1),
+            arg2_(v) {}
+    };
+
+    template <typename T0, typename T1, typename T2, typename T, typename... Ts>
+    struct Arg2<T0, T1, T2, T, Ts...> : public Arg2<T0,T1,T2,Ts...>
+    {
+        Arg2<T0,T1,T2,T,Ts...>(T0& arg0, T1& arg1, T2& v) :
+            Arg2<T0,T1,T2,Ts...>(arg0,arg1,v) {}
         virtual ArgList<TArgs...>* addArg(T& v) { return this; }
     };
+
 
 };
 
@@ -49,9 +87,25 @@ struct Builder
 template <typename... TArgs>
 template <typename T, typename... Ts>
 Builder<TArgs...>::ArgList<TArgs...>*
-Builder<TArgs...>::ArgStart<T, Ts...>::addArg(T& v)
+Builder<TArgs...>::ArgStart<T,Ts...>::addArg(T& v)
 {
     return new Arg0<T,TArgs...>(v);
+}
+
+template <typename... TArgs>
+template <typename T0, typename T, typename... Ts>
+Builder<TArgs...>::ArgList<TArgs...>*
+Builder<TArgs...>::Arg0<T0,T,Ts...>::addArg(T& v)
+{
+    return new Arg1<T0,T,TArgs...>(Arg0<T0>::arg0_,v);
+}
+
+template <typename... TArgs>
+template <typename T0, typename T1, typename T, typename... Ts>
+Builder<TArgs...>::ArgList<TArgs...>*
+Builder<TArgs...>::Arg1<T0,T1,T,Ts...>::addArg(T& v)
+{
+    return new Arg2<T0,T1,T,TArgs...>(Arg0<T0>::arg0_,Arg1<T0,T1>::arg1_,v);
 }
 
 } // namespace multi
